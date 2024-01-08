3891154195e668d3811cbc82b08c2658bd19a36c
Gwenn RT <gwenncreative@gwenns-MBP.attlocal.net>
2024-01-07 18:46:56 -0600

    added in js outline and changed weather image links in html

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// API Key
+var APIKey = '53cebe99512b622d37b87c20ca19d2a4';
+
+// clicking search button function
+
+// save search history to list
+var searchHistory = [];
+
+
+// search history display
+
+// current weather display
+
+// five day forecast display
+
+// clicking search history list item
+
+
+
+
