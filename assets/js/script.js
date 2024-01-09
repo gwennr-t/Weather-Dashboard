552619de38ca758b1d2c4dc213dac9771e332855
Gwenn RT <gwenncreative@gwenns-MBP.attlocal.net>
2024-01-08 18:02:20 -0600

    outlined js and added in api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@
 var APIKey = '53cebe99512b622d37b87c20ca19d2a4';
 
 // clicking search button function
+var cityInput= document.querySelector('#city-input');
+var searchButton= document.querySelector('#search-button');
+
+searchButton.addEventListener('click', getCityInfo);
 
 // save search history to list
 var searchHistory = [];
