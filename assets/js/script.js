e4c5b991ec666f86d748b89cc595eb2511bc91c7
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-01-27 14:26:35 -0600

    made more edits to js. not finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,21 @@
 var input = document.getElementById("city-input");
 var button = document.getElementById("search-button");
 var msgDiv = document.querySelector("#msg");
-var userSearchSpan = document.getElementById("search-history");
+var userSearchList = document.getElementById("search-history");
 var searches = [];
-
-// display messages for error or success
-function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute("class", type);
-  }
 
 // 
 function renderHistory () {
-    userSearchSpan.textContent = searches.length;
+    userSearchList.innerHTML = "";
 
-    for (var i = 0; i < searches.length; i++) {
+    for (var i = 0; i < searches.length ; i++) {
         var search = searches[i];
+        var newButton = document.createElement("button");
 
-        var li = document.createElement("li");
-        li.textContent = search;
+        newButton.textContent = search;
+        newButton.setAttribute("data-index", i);
+
+        userSearchList.appendChild(newButton);
     }
 }
 
@@ -30,6 +27,7 @@ function init() {
     if (storedSearches !== null) {
         searches = storedSearches;
     }
+
     renderHistory();
 }
 
@@ -40,69 +38,81 @@ function storeSearches() {
 button.addEventListener("click", function(event) {
     event.preventDefault();
 
-    var search = userSearchSpan.value;
+    var search = userSearchList.value;
+    var searchText = input.value.trim();
 
     if (search === "") {
-        displayMessage("error", "Please enter a valid city name.")
-    } else {
-        displayMessage("success", "Enjoy the weather!")
-
-        localStorage.setItem("search", searches);
-        renderHistory();
+        return;
     }
+
+    searches.push(searchText);
+    input.value = "";
+
+    storeSearches();
+    renderHistory();
 });
 
+init();
 
 // API key variable
 var apiKey = "53cebe99512b622d37b87c20ca19d2a4";
+var currentCity = document.querySelector(".current-city-date");
+var currentIcon = document.querySelector("#icon");
+var currentTemp = document.querySelector("#current-city-temperature");
+var currentWind = document.querySelector("#current-city-wind");
+var currentHumidity = document.querySelector("#current-city-humidity");
 
 // function to search API - Current Weather
 var currentWeather = function(){
-    var cityLat = response.coord.lat;
-    var cityLon = response.coord.lon;
-
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial")
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + apiKey + "&units=imperial")
         .then(function(response){
             return response.json();
         })
-        .then(function(response){
-            var currentCityDate = document.querySelector(".current-city-date");
-            var icon = document.querySelector("#icon");
-            var currentTemp = document.querySelector("#current-city-temperature");
-            var currentWind = document.querySelector("#current-city-wind");
-            var currentHumidity = document.querySelector("#current-city-humidity");
+        .then(function(data){
+            var currentCityDate = data["dt_txt"];
+            var icon = data["weather"][0]["icon"];
+            var currentTemp1 = data["main"]["temp"];
+            var currentWind1 = data["wind"]["speed"];
+            var currentHumidity1 = data["main"]["humidity"];
 
-            currentCityDate.innerHTML = response[""];
-            icon.innerHTML = response[""];
-            currentTemp.innerHTML = response[""];
-            currentWind.innerHTML = response[""];
-            currentHumidity.innerHTML = response[""];
-
+            currentCity.innerHTML = currentCityDate;
+            currentIcon.innerHTML = icon;
+            currentTemp.innerHTML = currentTemp1;
+            currentWind.innerHTML = currentWind1;
+            currentHumidity.innerHTML = currentHumidity1;
         })
-    console.log(response)
 }
 
+currentWeather();
+
 // function to search API - 5 Day Forecast
-var fiveDayForecast = function(){
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial")
+var futureDate = document.querySelector("#future-" + i);
+var futureIcon = document.querySelector("#future-icon-" + i);
+var futureTemp = document.querySelector("#future-temp-" + i);
+var futureWind = document.querySelector("#future-wind-" + i);
+var futureHumidity = document.querySelector("#future-humidity-" + i);
+
+var fiveDayForecast = function() {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + apiKey + "&units=imperial")
         .then(function(response){
             return response.json();
         }) 
         .then(function(data){
             for(var i = 0; i < 5; i++) {
-                var futureDate = document.querySelector("#future-" + i);
-                var futureIcon = document.querySelector("#future-icon-" + i);
-                var futureTemp = document.querySelector("#future-temp-" + i);
-                var futureWind = document.querySelector("#future-wind-" + i);
-                var futureHumidity = document.querySelector("#future-humidity-" + i);
-                
-                futureDate.innerHTML = response[""];
-                futureIcon.innerHTML = response[""];
-                futureTemp.innerHTML = response[""];
-                futureWind.innerHTML = response[""];
-                futureHumidity.innerHTML = response[""];
+                var futuredate1 = data["list"]["main"]["dt_text"];
+                var futureIcon1 = data["list"]["weather"]["icon"];
+                var futureTemp1 = data["list"]["main"]["temp"];
+                var futureWind1 = data["list"]["wind"]["speed"];
+                var futureHumidity1 = data["list"]["main"]["humidity"];
+
+                futureDate.innerHTML = futuredate1;
+                futureIcon.innerHTML = futureIcon1;
+                futureTemp.innerHTML = futureTemp1;
+                futureWind.innerHTML = futureWind1;
+                futureHumidity.innerHTML = futureHumidity1;
             }
         });
 
 }
 
+fiveDayForecast();
